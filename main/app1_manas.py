ad4513e9223cfc5719f1e8b33b9614cf21d6da72
Manas Bedekar <53872993+manasbede@users.noreply.github.com>
2021-02-25 13:40:22 +0000

    Update app1_manas.py

--- main/app1_manas.py
+++ main/app1_manas.py
@@ -1,5 +1,10 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,jsonify
+from flask_pymongo import PyMongo
+import json
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://apecskaam:apecs123@apecs.gucd9.mongodb.net/APECS_User_Data?retryWrites=true&w=majority"
+mongo=PyMongo(app)
 
 """@app.route('/')
 def hello_world():
@@ -12,7 +17,7 @@ def login():
       user = request.form['email']
       #name=request.form['name']
       if user=="manasbedekar002@gmail.com":
-          return render_template("Webmail.html")
+          return render_template("1.html")
       else:
           return render_template("signlog.html")
    else:
@@ -23,11 +28,11 @@ def register():
    if request.method == 'POST':
       user = request.form['email']
       name=request.form['name']
-      print(name)
-      if user=="manasbedekar002@gmail.com":
-          return render_template("Webmail.html")
-      else:
-          return render_template("sign.html")
+      password=request.form['password']
+
+      a=mongo.db.Log_Sign_Data.find_one({'name':name})
+      print(a['email'])
+      return redirect(url_for('login'))
    else:
       return render_template("sign.html")
 
